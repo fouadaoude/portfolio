7ef0ed2ef434195a35d9f55096041dfcd66be721
Fouad Aoude <aoude13@gmail.com>
2020-03-06 17:34:42 -0500

    All Files

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <title>Fouad Aoude</title>
+</head>
+<body>
+    <div class="img" 
+    style="width:100%;position:absolute;height:102%;top:0;left:0;">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Me</a></li>
+        <li><a href="#">Contact Me</a></li>
+        <li><a href="#">Login</a></li>
+        <li style="float:right"><a class="active" href="#">Register</a></li>
+    </ul>
+    </div>
+
+    <div class="about">
+    <h1>About Me</h1>
+    </div>
+</body>
+</html>
